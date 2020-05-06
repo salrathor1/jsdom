f0302de0f1b00c2a2db09b575a3c052174c46ffc
salrathor1 <salrathor1@hotmail.com>
2020-05-06 15:43:03 +0100

    practice 9 updated

--- 9 - Removing Elements/app.js
+++ 9 - Removing Elements/app.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('input');
 const button = document.getElementById('submit');
 /*add new const below*/
+const remove = document.getElementById("remove");
 
 /*from previous ex*/
 button.addEventListener('click', () => {
@@ -15,3 +16,13 @@ button.addEventListener('click', () => {
 })
 
 /*New code below*/
+
+remove.addEventListener("click", ()=> {
+    let lastItem = document.querySelector('li:last-child');
+    let list = document.getElementsByTagName('ul')[0];
+    list.removeChild(lastItem);
+})
+
+removeBtn.addEventListener('click', () => {
+
+    })
